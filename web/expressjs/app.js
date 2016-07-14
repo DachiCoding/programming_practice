6abf5bd65d25180b7da2d84405e261d23a364f9a
DachiCoding <dachi.xu@gmail.com>
2016-07-14 15:47:46 -0500

    practice repo update

--- web/expressjs/app.js
+++ web/expressjs/app.js
@@ -1,7 +1,13 @@
 const express = require("express");
+const morgan = require("morgan");
 const path = require("path");
 const fs = require("fs");
 const app = express();
+
+app.use(morgan("short"));
+
+const staticPath = path.join(__dirname,"static");
+app.use(express.static(staticPath));
 
 app.use(function(req,res,next){
     console.log("Request ID: " + req.url);
@@ -9,20 +15,8 @@ app.use(function(req,res,next){
     next();
 });
 
-app.use(function(req,res,next){
-    var filePath = path.join(__dirname,"static",req.url);
-    fs.stat(filePath, function(err, fileInfo){
-        if (err){
-            next();
-            return;
-        }
-
-        if (fileInfo.isFile()){
-            res.sendFile(filePath);
-        } else {
-            next();
-        }
-    });
+app.get("/olivia", function(req,res){
+    res.send("Welcome to Olivia's homepage!");
 });
 
 app.use(function(req,res){
